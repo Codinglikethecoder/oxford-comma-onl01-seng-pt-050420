4df27d8405db99527daf8153fb27b0bbbb2dbba4
Jean  Isidore <jnisidore@gmail.com>
2020-05-14 03:43:18 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -1,4 +1,6 @@
-oxford_comma =
+oxford_comma = ["kiwi", "durian"]
+
+oxfor_comma.split 
   it 'returns a string without any additional fomatting when given a 1-element array' do
     expect(oxford_comma(["kiwi"])).to eq("kiwi")
   end
